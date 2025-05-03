78fefc751c8c5e8682d5fd8897ca1cd961e0b763
digos <diogobissolimoreno@gmail.com>
2025-05-03 11:00:49 -0300

    10. incrementando teste de nome com Theory

--- cliente-solution-test/domain/ClienteTest.cs
+++ cliente-solution-test/domain/ClienteTest.cs
@@ -4,29 +4,30 @@ namespace cliente_solution_test.domain;
 
 public class ClienteTest
 {
-    //então vamos começar validando as regras do domínio
-    //nossa regra de negócio diz que um cliente precisa ter um nome, um documento e um email
-    //o nome não pode conter números e precisa ter no minimo 3 caracteres (olhar a classe Cliente)
-    //o documento deve conter apenas números e precisa ter no minimo 11 caracteres
-    //o email precisa conter um "@"
-
-    //vamos começar validando que o nome não deve conter números
-    [Fact]
-    public void Ao_criar_nao_deve_aceitar_numeros_no_nome()
+    /*
+        o [Fact] é para casos onde temos um cenário de teste.
+        se quisermos, e é recomendado, que testemos mais de um cenário de teste, podemos usar o [Theory]
+    */
+    [Theory]
+    [InlineData("123")] //teste que já tinhamos antes
+    [InlineData("1234W")] //letras e numeros
+    [InlineData("     ")]//espaco em branco
+    public void CriarCliente_NomeComNaoAlfabeticos_DeveLancarException(string nome_invalido)
     {
         //arrange
-        var nome_invalido = "123";
-        var documento = "12345678901"; //como estamos validando o nome primeiro, vamos deixar os demais parâmetros validos
+        var documento = "12345678901";
         var email = "teste@teste.com.br";
 
         //act & assert
         Assert.Throws<Exception>(() => new Cliente(nome_invalido, documento, email));
     }
 
-    //se rodarmos o teste, ele vai dar errado. isso porque não estamos validando o nome
-    //no construtor, apenas no método de atualização
+    //se rodarmos o teste, veremos que ele da certo. Ou seja... nossa classe de cliente não apenas não aceita números, como também valida espaços em branco
 
-    //se fizermos o mesmo teste para o método de atualização do nome ele dará certo
+    //então podemos renomear o teste para ficar mais claro qual de fato é nossa regra.
+    //sempre bom lembrar: testes também servem como documentação de regras
+    //Ao_criar_nao_deve_aceitar_numeros_no_nome -> CriarCliente_NomeComNaoAlfabeticos_DeveLancarException
+
 
     [Fact]
     public void Ao_Atualizar_nome_nao_deve_aceitar_numeros()
@@ -39,12 +40,6 @@ public class ClienteTest
         Assert.Throws<Exception>(() => cliente.AtualizarNome(nome_invalido)); //esse está correto
     }
 
-    //logo, o que o teste nos mostra é um furo na nossa lógica de negócio.
-    //ao criar um cliente eu posso colocar números no nome, mas ao editar o cliente não.
-
-    //vamos corrigir o furo na lógica do cliente
-
-    //implementando os outros testes
 
     [Fact]
     public void Ao_criar_nao_deve_aceitar_nomes_menores_que_3_caracteres()
