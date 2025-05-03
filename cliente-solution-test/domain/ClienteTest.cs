02d10928df2f5ab237fb8289e4cb6ccef6bff442
digos <diogobissolimoreno@gmail.com>
2025-05-03 10:51:48 -0300

    9. implementando os demais testes
    - agora temos todas as regras do cliente mapeadas, pórem, no próximo
    passo podemos fazer melhorias para deixar os teste melhores e mais
    abrangentes

--- cliente-solution-test/domain/ClienteTest.cs
+++ cliente-solution-test/domain/ClienteTest.cs
@@ -43,4 +43,101 @@ public class ClienteTest
     //ao criar um cliente eu posso colocar números no nome, mas ao editar o cliente não.
 
     //vamos corrigir o furo na lógica do cliente
+
+    //implementando os outros testes
+
+    [Fact]
+    public void Ao_criar_nao_deve_aceitar_nomes_menores_que_3_caracteres()
+    {
+        //arrange
+        var nome_invalido = "d";
+        var documento = "12345678901"; //como estamos validando o nome primeiro, vamos deixar os demais parâmetros validos
+        var email = "teste@teste.com.br";
+
+        //act & assert
+        Assert.Throws<Exception>(() => new Cliente(nome_invalido, documento, email));
+    }
+
+    [Fact]
+    public void Ao_Atualizar_nome_nao_deve_aceitar_nomes_menores_que_3_caracteres()
+    {
+        //arrange
+        var cliente = new Cliente("nome", "12345678901", "teste@teste.com.br");
+        var nome_invalido = "d";
+
+        //act & assert
+        Assert.Throws<Exception>(() => cliente.AtualizarNome(nome_invalido));
+    }
+
+    [Fact]
+
+    public void Ao_criar_nao_deve_aceitar_letras_no_documento()
+    {
+        //arrange
+        var nome = "nome";
+        var documento_invalido = "123456789a";
+        var email = "teste@teste.com.br";
+
+        //act & assert
+        Assert.Throws<Exception>(() => new Cliente(nome, documento_invalido, email));
+    }
+
+    [Fact]
+    public void Ao_Atualizar_documento_nao_deve_aceitar_letras()
+    {
+        //arrange
+        var cliente = new Cliente("nome", "12345678901", "teste@teste.com.br");
+        var documento_invalido = "123456789a";
+
+        //act & assert
+        Assert.Throws<Exception>(() => cliente.AtualizarDocumento(documento_invalido));
+    }
+
+    [Fact]
+    public void Ao_criar_nao_deve_aceitar_documentos_menores_que_11_caracteres()
+    {
+        //arrange
+        var nome = "nome";
+        var documento_invalido = "123456789";
+        var email = "teste@teste.com.br";
+
+        //act & assert
+        Assert.Throws<Exception>(() => new Cliente(nome, documento_invalido, email));
+    }
+
+    [Fact]
+    public void Ao_Atualizar_documento_nao_deve_aceitar_documentos_menores_que_11_caracteres()
+    {
+        //arrange
+        var cliente = new Cliente("nome", "12345678901", "teste@teste.com.br");
+        var documento_invalido = "123456789";
+
+        //act & assert
+        Assert.Throws<Exception>(() => cliente.AtualizarDocumento(documento_invalido));
+    }
+
+    [Fact]
+    public void Ao_criar_nao_deve_aceitar_emails_sem_arroba()
+    {
+        //arrange
+        var nome = "nome";
+        var documento = "12345678901";
+        var email_invalido = "email";
+
+        //act & assert
+        Assert.Throws<Exception>(() => new Cliente(nome, documento, email_invalido));
+    }
+
+    [Fact]
+    public void Ao_Atualizar_email_nao_deve_aceitar_emails_sem_arroba()
+    {
+        //arrange
+        var cliente = new Cliente("nome", "12345678901", "teste@teste.com.br");
+        var email_invalido = "email";
+
+        //act & assert
+        Assert.Throws<Exception>(() => cliente.AtualizarEmail(email_invalido));
+    }
+
+
 }
