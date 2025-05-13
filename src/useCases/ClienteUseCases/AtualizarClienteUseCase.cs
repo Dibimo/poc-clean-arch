6bc65af1de5ae2d58cd588e70cab2a373557cb70
digos <diogobissolimoreno@gmail.com>
2025-05-12 22:24:27 -0300

    15.1 - explicando porque a validação de objeto null é necessária

--- src/useCases/ClienteUseCases/AtualizarClienteUseCase.cs
+++ src/useCases/ClienteUseCases/AtualizarClienteUseCase.cs
@@ -9,44 +9,19 @@ public class AtualizarClienteUseCase(ClienteRepository repository)
 
     public void Executar(int id, string nome, string documento, string email)
     {
-        //no caso do atualizar, precisamos retornar exatamente o objeto que queremos atualizar
-        //então precisariamos de um "encontre por id", mas nossa interface de ClienteRepository não possi esse método nesse ponto
-        //simples de resolver, basta implementar
+        //buscando o cliente
         var cliente = _repository.BuscarPorId(id);
 
-        //estamos acostumados a fazer algo mais ou menos assim a partir daqui
-        /*
-            cliente.Nome = nome;
-            cliente.Documento = documento;
-            cliente.Email = email;
-            _repository.Atualizar(cliente);
-        */
+        //a interface estabelece que sempre deve ser retornado um cliente
+        //cliente é um objeto, logo, todo o objeto pode ser uma referencia nula
 
-        //mas lembram das regras de negócio?
-        //para garantir nossa regras de negócio, estamos acostumados a fazer algo assim:
-        /*
-            ValidarNome(nome);
-            ValidarDocumento(documento);
-            ValidarEmail(email);
 
-            cliente.Nome = nome;
-            cliente.Documento = documento;
-            cliente.Email = email;
-            _repository.Atualizar(cliente);
-        */
-
-        //ou qualquer outra variação parecida.
-        //o ponto é: nossa regra de negócio está fora do dominio (nossa classe cliente), então vamos ter problemas para atualizar e dar manutenção.
-        //fora que abrirmos margem para que essas funções não sejam chamadas e etc. etc. Problemas é o que não faltam e todos já sabemos disso.
-
-        //então, a forma como a arquitetura limpa propõe é:
-
-        //garanto as regras de negócio
+        //logo, esse código não vai funcionar caso o cliente não exista (cliente == null)
+        //vamos ter que atualizar os nossos testes para validar esse caso de erro
         cliente.AtualizarDocumento(documento);
         cliente.AtualizarEmail(email);
         cliente.AtualizarNome(nome);
 
-        //depois passo para a camada de persistencia
         _repository.Atualizar(cliente);
     }
 }
