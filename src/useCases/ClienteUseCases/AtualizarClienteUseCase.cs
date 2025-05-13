ecaefecd113bdca5e3bec2b5006257cb54fece4d
digos <diogobissolimoreno@gmail.com>
2025-05-12 22:57:33 -0300

    15.4 Implementando o lançamento da exceção corretamente
    Agora de rodarmos o teste implementado no commit anterior, vamos obter
    sucesso

--- src/useCases/ClienteUseCases/AtualizarClienteUseCase.cs
+++ src/useCases/ClienteUseCases/AtualizarClienteUseCase.cs
@@ -1,5 +1,6 @@
 using cliente_solution.domain.models;
 using cliente_solution.domain.repository;
+using cliente_solution.useCases.Exceptions;
 
 namespace cliente_solution.useCases.ClienteUseCases;
 
@@ -9,15 +10,9 @@ public class AtualizarClienteUseCase(ClienteRepository repository)
 
     public void Executar(int id, string nome, string documento, string email)
     {
-        //buscando o cliente
-        var cliente = _repository.BuscarPorId(id);
+        //implementando o lançamento da exceção corretamente
+        var cliente = _repository.BuscarPorId(id) ?? throw new EntidadeNaoExistente("cliente", id);
 
-        //a interface estabelece que sempre deve ser retornado um cliente
-        //cliente é um objeto, logo, todo o objeto pode ser uma referencia nula
-
-
-        //logo, esse código não vai funcionar caso o cliente não exista (cliente == null)
-        //vamos ter que atualizar os nossos testes para validar esse caso de erro
         cliente.AtualizarDocumento(documento);
         cliente.AtualizarEmail(email);
         cliente.AtualizarNome(nome);
