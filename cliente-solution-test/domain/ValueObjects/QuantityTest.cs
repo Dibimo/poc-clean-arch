687b41bc5a21bede8e24b74f5696458d3946df26
digos <diogobissolimoreno@gmail.com>
2025-06-01 15:45:39 -0300

    test: implmentando testes para Parse de Quantidade

--- cliente-solution-test/domain/ValueObjects/QuantityTest.cs
+++ cliente-solution-test/domain/ValueObjects/QuantityTest.cs
@@ -24,4 +24,48 @@ public class QuantityTest
         Assert.Throws<ArgumentException>(() => new Quantity(-1));
     }
 
+
+    [Theory]
+    [InlineData(0)]
+    [InlineData(1)]
+    public void Parse_ValidQuantities_ShoudCreateSuccessfully(int value)
+    {
+        // Act
+        var quantity = Quantity.Parse(value);
+
+        // Assert
+        Assert.Equal(value, quantity.Value);
+    }
+
+    [Fact]
+    public void Parse_InvalidQuantity_ShoudThrowArgumentException()
+    {
+        // Act & Assert
+        Assert.Throws<ArgumentException>(() => Quantity.Parse(-1));
+    }
+
+    [Theory]
+    [InlineData("1", 1)]
+    [InlineData("00010", 10)]
+    [InlineData("0000", 0)]
+    [InlineData("   1   ", 1)]
+    public void Parse_ValidString_ShouldCreateSuccessfully(string quantityString, int quantityValue)
+    {
+        // Act
+        var quantity = Quantity.Parse(quantityString);
+
+        // Assert
+        Assert.Equal(quantityValue, quantity.Value);
+    }
+
+    [Theory]
+    [InlineData("invalid")]
+    [InlineData("1.1")]
+    [InlineData("0001.1")]
+    [InlineData("   1.1   ")]
+    public void Parse_InvalidString_ShouldThrowArgumentException(string quantityString)
+    {
+        // Act & Assert
+        Assert.Throws<ArgumentException>(() => Quantity.Parse(quantityString));
+    }
 }
